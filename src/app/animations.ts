f512b826a7caea2267dd079d87e8bba4b0a8b2b0
Jakub Faliszewski <jfaliszewski@interia.pl>
2019-06-10 00:35:29 +0200

    accesibility, images paths, optim

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,38 +1,120 @@
 import { trigger, transition, style, query, animateChild, group, animate, } from '@angular/animations';
 
-
-
 export const slider =
   trigger('routeAnimations', [
-    transition('* => isLeft', slideInAnimation('left')),
-    transition('* => isRight', slideInAnimation('right')),
-    transition('isRight => *', slideInAnimation('left')),
-    transition('isLeft => *', slideInAnimation('right'))
-  ]);
-
-function slideInAnimation(direction) {
-  const optional = { optional: true };
-  return [
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        [direction]: 0,
-        width: '100vw',
-        zIndex: -1
-      })
-    ], optional),
-    query(':enter', [
-      style({ [direction]: '-100%', position: 'fixed' })
-    ]),
-    group([
-      query(':leave', [
-        animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ [direction]: '100%' }))
-      ], optional),
+    transition('* => isLeft', [
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          zIndex: -1
+        })
+      ], { optional: true }),
       query(':enter', [
-        animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ [direction]: '0%' }))
+        style({ left: '-100%', position: 'fixed' })
+      ]),
+      group([
+        query(':leave', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ left: '100%' }))
+        ], { optional: true }),
+        query(':enter', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ left: '0%' }))
+        ])
+      ])
+    ]),
+    transition('* => isRight', [
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: '100vw',
+          zIndex: -1
+        })
+      ], { optional: true }),
+      query(':enter', [
+        style({ right: '-100%', position: 'fixed' })
+      ]),
+      group([
+        query(':leave', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ right: '100%' }))
+        ], { optional: true }),
+        query(':enter', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ right: '0%' }))
+        ])
+      ])
+    ]),
+    transition('isRight => *', [
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          zIndex: -1
+        })
+      ], { optional: true }),
+      query(':enter', [
+        style({ left: '-100%', position: 'fixed' })
+      ]),
+      group([
+        query(':leave', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ left: '100%' }))
+        ], { optional: true }),
+        query(':enter', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ left: '0%' }))
+        ])
+      ])
+    ]),
+    transition('isLeft => *', [
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: '100vw',
+          zIndex: -1
+        })
+      ], { optional: true }),
+      query(':enter', [
+        style({ right: '-100%', position: 'fixed' })
+      ]),
+      group([
+        query(':leave', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ right: '100%' }))
+        ], { optional: true }),
+        query(':enter', [
+          animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ right: '0%' }))
+        ])
       ])
     ])
-  ];
-}
+  ]);
+
+//export function slideInAnimation(direction) {
+//  const optional = { optional: true };
+//  return [
+//    query(':enter, :leave', [
+//      style({
+//        position: 'absolute',
+//        top: 0,
+//        [direction]: 0,
+//        width: '100vw',
+//        zIndex: -1
+//      })
+//    ], optional),
+//    query(':enter', [
+//      style({ [direction]: '-100%', position: 'fixed' })
+//    ]),
+//    group([
+//      query(':leave', [
+//        animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ [direction]: '100%' }))
+//      ], optional),
+//      query(':enter', [
+//        animate('1000ms cubic-bezier(0.65, 0.05, 0.29, 1.03)', style({ [direction]: '0%' }))
+//      ])
+//    ])
+//  ];
+//}
 
